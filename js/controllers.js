0c80563083b484463c7cc89be0995a5beaef18a5
bhannat2012 <bhannat2012@gmail.com>
2013-12-27 12:07:59 +0530

    tempalate-added

--- js/controllers.js
+++ js/controllers.js
@@ -1,41 +1,33 @@
 'use strict';
 
-angular.module('myApp.controllers', [])
-    .controller('MainCtrl', ['$scope', '$rootScope', '$window', '$location', function ($scope, $rootScope, $window, $location) {
+angular.module('myApp.controllers', [])    .
+    constant('SERVER','http://localhost:4002/json')    .
+    controller('MainCtrl', ['$scope', '$rootScope', '$window', '$location', function ($scope, $rootScope, $window, $location) {
         $scope.slide = '';
         $rootScope.back = function() {
           $scope.slide = 'slide-right';
           $window.history.back();
-        }
+        };
         $rootScope.go = function(path){
           $scope.slide = 'slide-left';
           $location.url(path);
         }
-    }])
-    .controller('HomeCtrl', ['$scope', 'Employee', function ($scope, Employee) {
+    }]).
+    controller('HomeCtrl', ['$scope', 'Employee', function ($scope, Employee) {
 
-    }]).controller('ChartCtrl', ['$scope', 'Employee', function ($scope, Employee) {
-        debugger;
+    }]).controller('ChartCtrl', ['$scope', 'SERVER','$http','$location', function ($scope, server,$http,$location) {
+        //debugger;
+
+        init();
+
         $scope.data = [[[0, 1], [1, 5], [2, 2]]];
-        $scope.floatData = [
-            {"label": "Series1", "data": 44},
-            {"label": "Series2", "data": 51},
-            {"label": "Series3", "data": 46},
-            {"label": "Series4", "data": 89},
-            {"label": "Series5", "data": 85}
-        ];
-
-//        $scope.floatData = [
-//            {"label": "Global", "data": 44},
-//            {"label": "Domestic", "data": 51},
-//        ];
-        /*$scope.canvasData = [
-            {  y: 6, legendText:"Google", label1: "83.3%" },
-            {  y: 8, legendText:"Yahoo!", label1: "8.16%" },
-            {  y: 9, legendText:"Bing", labe1: "4.67%" },
-            {  y: 4, legendText:"Baidu" , label1: "1.67%"},
-            {  y: 5, legendText:"Baidu11" , label1: "1.67%"}
-
+        $scope.floatData = [];
+        /*[
+            {"label": "Series1", "data": 44,clickable: true,more:'more-data1'},
+            {"label": "Series2", "data": 51,clickable: true,more:'more-data2'},
+            {"label": "Series3", "data": 46,clickable: true,more:'more-data3'},
+            {"label": "Series4", "data": 89,clickable: true,more:'more-data4'},
+            {"label": "Series5", "data": 85,clickable: true,more:'more-data5'}
         ];*/
 
         $scope.canvasData = [
@@ -43,9 +35,42 @@ angular.module('myApp.controllers', [])
             {  y: 8, legendText:"Global", label1: "(Global)8.16%" }
 
         ];
-        $scope.floatOnSeriesClick=function(){
-            debugger;
 
+        $scope.floatOnSeriesClick=function(flot){
+            debugger;
+           // alert(flot.series);
+/*
+            console.log(JSON.stringify(flot.series));
+
+            var seriesJSON = $scope.floatData[flot.seriesIndex].nextUrl;
+            var url=  server + '/'+seriesJSON+'.json';
+            loadData(url);
+*/
+
+            $location.path(  server +  '/employees' )
+
+        };
+
+        function init(){
+            debugger;
+            var url = server + '/getData.json';
+            loadData(url);
+            //$scope.floatData
+        }
+
+        function loadData(url){
+            $http.get(url).success(
+                function(data, status, headers, config) {
+                    debugger;
+                    $scope.floatData = data;
+                    // this callback will be called asynchronously
+                    // when the response is available
+                }).
+                error(function(data, status, headers, config) {
+                    debugger;
+                    // called asynchronously if an error occurs
+                    // or server returns response with an error status.
+                });
         }
 
     }])
--- js/flot-directive.js
+++ js/flot-directive.js
@@ -2,62 +2,96 @@
 
 angular.module('flotD', [])
     .directive('flotChart', function () {
+        var htmlTemplate = '<div></div>';
+
         return {
-            restrict: 'A',
+            template: htmlTemplate,
+            restrict: 'E',
+            replace: true,
 //            scope:{
 //                floatData:'=',
 //                floatOnSeriesClick :'='
 //            },
             link: function (scope, charDiv, attrs) {
-                debugger;
+               // debugger;
 
                 var chart = null,
                     opts = { };
 
                 var data = scope[attrs.ngModel];
-                var onSeriesClicFn = scope[attrs.floatOnSeriesClick];
+                var onSeriesClickFn = scope[attrs.floatOnSeriesClick];
+                var options = {
+
+                    series: {
+                        pie: {
+                            show: true,
+                            radius: 1,
+                            label: {
+                                show: true,
+                                radius: 3 / 4,
+                                formatter: function (label, series) {
+
+                                    return "<div style='font-size:8pt; text-align:center; padding:2px; color:white;'>" + label + "<br/>" + Math.round(series.percent) + "%</div>";
+                                },
+                                background: {
+                                    opacity: 0.5,
+                                    color: '#000'
+                                }
+                            }
+                        }
+                    },
+                    legend: {
+                        show: false
+                    },
+                    grid: {
+                        hoverable: true,
+                        clickable: true
+                    },zoom: {
+                        interactive: true
+                    },
+                    pan: {
+                        interactive: true
+                    }
+                };
+                var chart = null;
+
                 scope.$watch('floatData', function (v) {
                    // chart = $.plot(elem, v , options);
-                    charDiv.unbind();
-                    debugger;
-                          $.plot(charDiv, v, {
+                    jQuery(charDiv).unbind();
+                   // debugger;
+                    if(!chart){
+                      chart =  $.plot(charDiv, v, options);
+                        debugger;
+                      //if(charDiv.show)  charDiv.show();
+                    }else{
+                        chart.setData(v);
+                        chart.setupGrid();
+                        chart.draw();
+                    }
 
-                            series: {
-                                pie: {
-                                    show: true,
-                                    radius: 1,
-                                    label: {
-                                        show: true,
-                                        radius: 3/4,
-                                        formatter:  function(label, series) {
-                                            debugger;
-                                            return "<div style='font-size:8pt; text-align:center; padding:2px; color:white;'>" + label + "<br/>" + Math.round(series.percent) + "%</div>";
-                                        },
-                                        background: {
-                                            opacity: 0.5,
-                                            color: '#000'
-                                        }
-                                    }
-                                }
-                            },
-                              legend: {
-                                  show: false
-                              },
-                            grid: {
-                                hoverable: true,
-                                clickable: true
-                            }
-                        });
 
-                    jQuery(charDiv.id).bind("plotclick", function(event, pos, obj) {
+                    jQuery(charDiv).bind("plotclick", function(event, pos, flotObject) {
+                       // debugger;
+                        if (!flotObject) {
+                            return;
+                        }
+                        if(typeof onSeriesClickFn === 'function' ){
+                          //  debugger;
+                            onSeriesClickFn(flotObject)
+                        }
+                    });
+                    /*charDiv.bind("plotclick", function(event, pos, obj) {
                         debugger;
                     });
-                    charDiv.bind("plotclick", function(event, pos, obj) {
+                    charDiv.bind("plotclick", function (event, pos, item) {
                         debugger;
-                    });
-                    charDiv.bind("click", function(event, pos, obj) {
+                    });*/
+                   /* charDiv.bind("click", function(event, pos, obj) {
                         debugger;
-                    });
+                        event.preventDefault();
+                    });*/
+
+/*
                     charDiv.bind("plotclick", function(event, pos, obj) {
 
 //                        if (!obj) {
@@ -74,6 +108,7 @@ angular.module('flotD', [])
                       }
                      //
                     });
+*/
                 });
             }
         }
